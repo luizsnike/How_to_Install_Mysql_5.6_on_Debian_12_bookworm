68b72cc72c3e0a7219fbd432e36c711e3ea20d49
Luiz Ribeiro <luizribeiro@outlook.com.br>
2024-05-29 22:23:44 -0300

    first commit

---
+++ Install_auto.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+if [ $UID -ne 0 ]; then
+    echo "Run as root"
+    exit 1
+fi
+DB_ROOT_PASSWD='pswd'
+DB_BASE_FOLDER='/dados/mysql'
+MYSQL_FILE='mysql-5.6.23-linux-glibc2.5-x86_64.tar.gz'
+apt update
+apt install libaio1 libncursesw5 libncurses5 wget -y
+cd /usr/local
+if [ -e "$MYSQL_FILE" ] ; then
+        mv $MYSQL_FILE mysql.tar.gz
+else
+        wget "https://downloads.mysql.com/archives/get/p/23/file/$MYSQL_FILE"
+fi
+groupadd mysql
+useradd -g mysql mysql
+mv $MYSQL_FILE mysql.tar.gz
+tar -xvf mysql.tar.gz
+rm mysql.tar.gz
+mv mysql-* mysql
+chown root:root mysql
+cd mysql
+chown -R mysql:mysql *
+mkdir -p "$DB_BASE_FOLDER/log/"
+cp -R data/ "$DB_BASE_FOLDER/data/"
+chown -R mysql:mysql $DB_BASE_FOLDER/
+sed -i 's,basedir=foo,basedir=/usr/local/mysql,g' /usr/local/mysql/scripts/mysql_install_db
+sed -i "s,datadir=bar,datadir=$DB_BASE_FOLDER/data/,g" /usr/local/mysql/scripts/mysql_install_db
+scripts/mysql_install_db --user=mysql --datadir=$DB_BASE_FOLDER/data/
+chown -R root .
+chown -R mysql:mysql $DB_BASE_FOLDER/
+cp support-files/my-default.cnf /etc/my.cnf
+sed -i 's,# basedir = .....,basedir=/usr/local/mysql,g' /etc/my.cnf
+sed -i "s,# datadir = .....,datadir=$DB_BASE_FOLDER/data/,g" /etc/my.cnf
+sed -i 's,# port = .....,port=3306,g' /etc/my.cnf
+sed -i 's,# socket = .....,socket=/tmp/mysql.sock,g' /etc/my.cnf
+echo 'pid-file=/tmp/mysqld.pid' >> /etc/my.cnf
+echo "log-error=$DB_BASE_FOLDER/log/log-error.log" >> /etc/my.cnf
+bin/mysqld_safe --user=mysql & cp support-files/mysql.server /etc/init.d/mysql.server
+sleep 5
+/etc/init.d/mysql.server start
+sleep 5
+bin/mysqladmin -u root password "$DB_ROOT_PASSWD"
+sleep 5
+/etc/init.d/mysql.server stop
+sleep 5
+update-rc.d -f mysql.server defaults
+ln -s /usr/local/mysql/bin/mysql /usr/local/bin/mysql
+ln -s /usr/local/mysql/bin/mysqldump /usr/local/bin/mysqldump
+sleep 5
+/etc/init.d/mysql.server start
+sleep 5
+/etc/init.d/mysql.server status
+echo "Process finished, you now have mysql with the basic configurations installed. Enjoy."
---
+++ README.md
@@ -0,0 +1,26 @@
+# English
+Script to install Mysql 5.6 on Debian 12.
+Use at your own risk, it is not recommended to run directly on a production server, install on a test VM and adjust as necessary and then run on your server.
+To use, simply download and change the variables to the values ​​you want.
+This script is used to install Mysql version 5.* using a custom folder to place the data and log.
+It has been tested on Debian 12.5 and Mysql 5.6.23.
+It was made to be as simple and easy to understand, adjust as needed.
+I hope it is useful for you.
+
+# Português
+Script para instalar o Mysql 5.6 no Debian 12.
+Use por sua conta e risco, não é recomendável executar direto em um servidor de produção, faça a instalação em um VM de teste e ajuste conforme necessário para só então executar no seu servidor.
+Para usar basta baixar e trocar as váriaveis pelos valores que deseja.
+Este script serve para instalar o Mysql versão 5.* utilizando uma pasta personalisada para colocar os dados e o log.
+Foi testado no Debian 12.5 e Mysql 5.6.23.
+Foi feito para ser o mais simples e de fácil entendimento, ajuste como necessitar.
+Espero que seja útil para você.
+
+# Spanish
+Script para instalar Mysql 5.6 en Debian 12.
+Úselo bajo su propio riesgo, no se recomienda ejecutarlo directamente en un servidor de producción, instalarlo en una máquina virtual de prueba y ajustar según sea necesario y luego ejecutarlo en su servidor.
+Para usarlo, simplemente descargue y cambie las variables a los valores que desee.
+Este script se utiliza para instalar Mysql versión 5.* usando una carpeta personalizada para colocar los datos y el registro.
+Ha sido probado en Debian 12.5 y Mysql 5.6.23.
+Fue creado para ser simple y fácil de entender, ajústelo según sea necesario.
+Espero que te sea útil.
